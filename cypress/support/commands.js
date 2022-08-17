7a4660f5286a692eab6e88b2c48e12ab92a2920f
Jeztc <dagreenrs@gmail.com>
2022-08-17 13:47:00 +0300

    fix: change the port to 3001

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -3,7 +3,7 @@ Cypress.Commands.add('login', ({ username, password }) => {
     username, password
   }).then(({ body }) => {
     localStorage.setItem('loggedBlogappUser', JSON.stringify(body))
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3001')
   })
 })
 
@@ -17,5 +17,5 @@ Cypress.Commands.add('createBlog', ({ title, author, url, likes }) => {
     }
   })
 
-  cy.visit('http://localhost:3000')
+  cy.visit('http://localhost:3001')
 })
